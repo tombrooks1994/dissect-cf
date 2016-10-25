9e81639f66db3428cd38edc43cd4c4ceb139f598
Tom Brooks <t.s.brooks@2013.ljmu.ac.uk>
2016-10-25 01:21:58 +0100

    Adding more values of a device into the code

--- src/main/java/hu/mta/sztaki/lpds/cloud/simulator/iaas/virtualMachineComponent.java
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/iaas/virtualMachineComponent.java
@@ -15,10 +15,22 @@ public class virtualMachineComponent {
 	// detection software because all users will not need to use the failure detection
 	// software. 
 	
-	int CPU = 0;
-	int GPU = 0;
-	int hardDrive = 0;
-	int SSD = 0;  
+	// Need to test for failures in more than just an average desktop going to be testing
+	// for other devices such as tablets, phones, networks and networking devices.
+	
+	int CPU = 0; 
+	int GPU = 0; // Up to 2 for testing purposes
+	int hardDrive = 0; // Up to 10 for testing purposes (4 for each device)
+	int SSD = 0;  // Up to 10 for testing purposes 
+	int RAM = 0; // Goes up to 4 per device
+	int powerSupply = 0;
+	int moBo = 0;
+	int fans = 0; // up to 4, can be more for testing
+	int wireless = 0;
+	int ethernet = 0; 
+	int audio = 0; // speakers or other output devices
+	int USB = 0; // depending on how many
+	int display = 0; // depending on port VGA, HDMI, DVI
 	
 	{
 		
@@ -42,34 +54,53 @@ public class virtualMachineComponent {
 	
 	if (CPU == 0) {
 		
-		System.out.print("GPU is either none existent or broken");
+		System.out.print("CPU is either none existent or broken");
 		
 	} else {
 		
-		System.out.println("GPU is in a working condition");
+		System.out.println("CPU is in a working condition");
 		
 	}
 	
 	if (hardDrive == 0) {
 		
-		System.out.print("GPU is either none existent or broken");
+		System.out.print("Hard Drive is either none existent or broken");
 		
 	} else {
 		
-		System.out.println("GPU is in a working condition");
+		System.out.println("Hard Drive is in a working condition");
 		
 	}
 	
 	if (SSD == 0) {
 		
-		System.out.print("GPU is either none existent or broken");
+		System.out.print("SDD is either none existent or broken");
 		
 	} else {
 		
-		System.out.println("GPU is in a working condition");
+		System.out.println("SSD is in a working condition");
 		
 	}
 	
+	if (moBo != 0) {
+		
+		boolean deviceFailed = false;
+		
+		if (deviceFailed == false) {
+			
+			System.out.println("Motherboard is working correctly, test for other failures!");
+			
+		} else {
+			
+			if (deviceFailed == true) {
+				
+			System.out.println("Motherboard failed, please replace motherboard!");
+				
+			}
+			
+		}
+		
+	}
 	
 }
 }
